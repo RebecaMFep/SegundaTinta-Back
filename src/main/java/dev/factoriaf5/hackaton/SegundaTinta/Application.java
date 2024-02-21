31ba55ee87d62f5fa42c752a1428842514ab9b07
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-21 22:04:33 +0100

    added dependencies

---
+++ src/main/java/dev/factoriaf5/hackaton/SegundaTinta/Application.java
@@ -0,0 +1,13 @@
+package dev.factoriaf5.hackaton.SegundaTinta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
