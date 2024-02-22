bda967da470d00cc0331151f6da386dfaec919a9
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-22 21:26:47 +0100

    added changes

--- src/main/java/dev/factoriaf5/hackaton/SegundaTinta/confing/SecurityConfiguration.java
+++ src/main/java/dev/factoriaf5/hackaton/SegundaTinta/confing/SecurityConfiguration.java
@@ -1,5 +1,15 @@
 package dev.factoriaf5.hackaton.SegundaTinta.confing;
 
-public class SecurityConfiguration {
-    
-}
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+
+
+
