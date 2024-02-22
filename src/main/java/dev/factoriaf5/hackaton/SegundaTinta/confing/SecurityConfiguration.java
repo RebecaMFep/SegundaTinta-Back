dcf2b9f3294cb2481ccb1ee4e6f8a1ae582d728b
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-22 22:35:35 +0100

    lasts changes, no coonected to h2 data base

--- src/main/java/dev/factoriaf5/hackaton/SegundaTinta/confing/SecurityConfiguration.java
+++ src/main/java/dev/factoriaf5/hackaton/SegundaTinta/confing/SecurityConfiguration.java
@@ -1,13 +1,6 @@
 package dev.factoriaf5.hackaton.SegundaTinta.confing;
 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.SecurityFilterChain;
-
 
 
 
