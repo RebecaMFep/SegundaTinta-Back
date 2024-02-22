1403c065bc1a7405ecd73b82cfc1ae65d592916a
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-22 18:02:58 +0100

    problems solved

--- src/main/java/dev/factoriaf5/hackaton/SegundaTinta/books/BookController.java
+++ src/main/java/dev/factoriaf5/hackaton/SegundaTinta/books/BookController.java
@@ -1,49 +1,8 @@
 package dev.factoriaf5.hackaton.SegundaTinta.books;
 
-import dev.factoriaf5.hackaton.SegundaTinta.books.BookService;
-
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatusCode;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-
-// import java.util.List;
-// import java.util.Optional;
-    
-
-
-// @RestController
-// @RequestMapping(path = "${api-endpoint}/books")
-// public class BookController {
-
-//     private final BookService bookService;
-
-//     @Autowired
-//     public BookController(BookService bookService) {
-//         this.bookService = bookService;
-//     }
-
-//     // Obtener todos los libros
-//     @GetMapping(path = "")
-//     public List<Book> index() {
-
-//         List<Book> books = service.getAll();
-//         return books;
-//     }
-
-//     // Obtener un libro por ID
-//     @GetMapping(path = "/{id}")
-//       public ResponseEntity<Book> getOneById(@PathVariable("id") Long id) throws Exception {
-
-//         Book book = service.getById(id);
-
-//         return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(book);
-//     }
-//     }
-
  
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -68,12 +27,12 @@ public class BookController {
 
     @GetMapping(path = "")
     public List<Book> index() {
-        return service.getAll();
+        return service.getAllBooks();
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<Book> getOneById(@PathVariable("id") Long id) {
-        Book book = service.getById(id);
+        Book book = service.getBookById(id);
         return new ResponseEntity<>(book, HttpStatus.OK);
     }
 } 
